62bc0c83509096e10d6bbdee904fb6f43c866f7b
uglyoholioh <87862128+uglyoholioh@users.noreply.github.com>
2022-01-06 22:49:16 +0800

    2249 test

---
+++ BizAppDev/BizAppDev/CustomerLanding.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/CustomerMain.Master" AutoEventWireup="true" CodeBehind="CustomerLanding.aspx.cs" Inherits="BizAppDev.CustomerLanding" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
--- BizAppDev/BizAppDev/CustomerMain.Master.designer.cs
+++ BizAppDev/BizAppDev/CustomerMain.Master.designer.cs
@@ -24,6 +24,15 @@ namespace BizAppDev
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
 
         /// <summary>
+        /// bootstrap-css control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlLink bootstrap-css;
+        
+        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
