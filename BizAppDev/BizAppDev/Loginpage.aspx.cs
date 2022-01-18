4e2528a93d9bd7fec825b87243103a971137775c
uglyoholioh <87862128+uglyoholioh@users.noreply.github.com>
2022-01-18 23:56:38 +0800

    plsletmego

--- BizAppDev/BizAppDev/Loginpage.aspx.cs
+++ BizAppDev/BizAppDev/Loginpage.aspx.cs
@@ -14,10 +14,9 @@ namespace BizAppDev
         protected void Login_Btn_Click(object sender, EventArgs e)
         {
             SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Project"].ToString());
-<<<<<<< HEAD
-
 =======
->>>>>>> parent of a1a92a5 (workinglogin)
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["HealthDBContext"].ToString());
+>>>>>>> Stashed changes
             Employee emp = new Employee();
             string queryStr = "SELECT * FROM Employee WHERE EmpID = @EmpID AND Password = @Password";
             SqlCommand cmd = new SqlCommand(queryStr, con);
