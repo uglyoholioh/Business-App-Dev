754ae50735bbe8f725e548b181585fcbb12c58d4
xorh699 <ongruihao1996@gmail.com>
2022-02-15 23:14:05 +0800

    latest

--- BizAppDev/BizAppDev/success.aspx.cs
+++ BizAppDev/BizAppDev/success.aspx.cs
@@ -42,8 +42,8 @@ namespace BizAppDev
                 {
                     Label3.Text = row.Field<decimal>("discountedtotal").ToString();
                 }
+              
                 Label1.Text = row.Field<string>("orderid").ToString();
-               
                 Label2.Text = row.Field<string>("email").ToString();
                 break;
          
